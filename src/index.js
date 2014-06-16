0e12e920a2f88280e8eaf56de052a2c8ee7ca6b4
Brian Di Palma <offler@gmail.com>
2014-06-16 18:48:34 +0100

    Load data using ES6 Generators and co package.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import co from "co";
+
+export function loadData2() {
+	co(loadDataGen())();
+}
+
+function *loadDataGen() {
+	try {
+		var loginJson = yield get('data/login-user-pass.json');
+
+		console.log(JSON.parse(loginJson));
+	} catch(loginError) {
+		console.error(loginError);
+	}
+
+	var contactsJson = yield get('data/contacts-user.json');
+	var chatMessagesJson = yield get('data/chat-messages.json');
+	var recentMessages = yield get('data/recent-messages-user.json');
+
+	console.log(JSON.parse(contactsJson));
+	console.log(JSON.parse(chatMessagesJson));
+	console.log(JSON.parse(recentMessages));
+}
+
 export function loadData() {
 	get('data/login-user-pass.json')
 		.then((loginJSON) => {
