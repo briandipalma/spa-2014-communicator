01a9a7419d8d7e1be90fd504d6db939477466eff
Brian Di Palma <offler@gmail.com>
2014-06-22 22:53:02 +0100

    Add step4 Promises based XHR requests and logging.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
 export function greet() {
     alert('Hello world');
 }
+
+export function loadData() {
+	get('/data/login-user-pass.json')
+		.then(function(response) {
+			console.log(response);
+		});
+}
+
+function get(url) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.send();
+
+	var promise = new Promise((resolve, reject) => {
+		xhr.onload = () => {
+			if (xhr.status === 200) {
+				resolve(xhr.response);
+			} else if (xhr.status === 404) {
+				reject(new Error(`Requested URL (${url}) unavailable`));
+			}
+		}
+	});
+
+	return promise;
+}
